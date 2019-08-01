548b006f6df6d76d728430323569d07bca982a45
NiRongFei <fei332085762@aliyun.com>
2019-08-01 15:31:05 +0800

    提交部署

---
+++ README.md
@@ -0,0 +1,4 @@
+## express-blog
+
+#### A blog demo based on node express and mongodb.
+
---
+++ config/production.js
@@ -0,0 +1,4 @@
+module.exports = {
+  port: 80,
+  mongodb: 'mongodb://username:password@ds139327.mlab.com:39327/blog'
+}
